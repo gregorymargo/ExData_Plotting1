46d4fdd14a016217af035857bc30d352ccf6bdbd
Gregory Margo <gmargo@yahoo.com>
2014-05-08 08:29:56 -0700

    Modify plot1 into a function.

--- plot1.R
+++ plot1.R
@@ -7,30 +7,34 @@
 # Exploratory Data Analysis
 # Course Project 1, Plot 1
 
+plot1 <- function(outFileName) {
 
-# Use common data reading function between plot?.R files.
-# See "read_data.R" for this common code.
-source("read_data.R")
-data <- read_data()
+    # Use common data reading function between plot?.R files.
+    # See "read_data.R" for this common code.
+    source("read_data.R")
+    dat <<- read_data()    # Push data up into enclosing environment for debug purposes
+
+
+    png(filename = outFileName,             # Specify output filename
+        width = 480, height = 480,          # Size is 480x480 pixels
+        #bg = "transparent"                  # Background is transparent, for submission
+        bg = "white"                        # Background is white, for testing
+        )
+
+    # Generate it baby!
+    with(dat,
+        hist(Global_active_power,
+            main="Global Active Power",
+            col="#FF2500",
+            xlab="Global Active Power (kilowatts)",
+            ylab="Frequency"))
+
+    # Close it out and write the file
+    ignore <- dev.off()                     # Capture the output to shut it up
+}
 
 outFileName <- "plot1.png"
-
-png(filename = outFileName,             # Output filename is "plot1.png"
-    width = 480, height = 480,          # Size is 480x480 pixels
-    #bg = "transparent"                  # Background is transparent, for submission
-    bg = "white"                        # Background is white, for testing
-    )
-
-# Generate it baby!
-with(data,
-    hist(Global_active_power,
-        main="Global Active Power",
-        col="#FF2500",
-        xlab="Global Active Power (kilowatts)",
-        ylab="Frequency"))
-
-# Close it out and write the file
-ignore <- dev.off()                     # Capture the output to shut it up
+timer <- system.time(plot1(outFileName))
 
 # Howdy
-print(paste0("Plot 1 written to ", outFileName))
+print(paste0("Plot 1 written to ", outFileName, " in ", timer["user.self"], " user seconds"))
