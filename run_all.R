77fa41cf497cfabb5854b14750c4f34aece8583b
Gregory Margo <gmargo@yahoo.com>
2014-05-08 13:42:16 -0700

    Add R script to generate all images.

---
+++ run_all.R
@@ -0,0 +1,17 @@
+
+# vim: set ts=8 sts=4 sw=4 et ft=r :
+#
+# Thu May 08 13:31:29 PDT 2014
+# Gregory Margo
+#
+# Exploratory Data Analysis
+# Course Project 1
+#
+# Generate all images
+
+suppressWarnings(file.remove(c("plot1.png", "plot2.png", "plot3.png", "plot4.png")))
+
+source("plot1.R")
+source("plot2.R")
+source("plot3.R")
+source("plot4.R")
